edf2c02a21d0f802acc44f3131dfd1009fdd404b
Nathan George Nascimento Gencissk <nathangngencissk@gmail.com>
2020-11-11 22:10:08 -0300

    Add route

--- src/routes/title.js
+++ src/routes/title.js
@@ -6,5 +6,6 @@ router.get('/get', titleController.getAll);
 router.post('/add', titleController.add);
 router.put('/edit/:id', titleController.edit);
 router.delete('/delete/:id', titleController.delete);
+router.post('/pay/:id', titleController.pay);
 
 module.exports = router;
